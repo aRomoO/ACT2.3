3dd264318bac63aa922a26f199b2ebc5557cef16
AbrahmCh1 <achavez@duxxsports.com.mx>
2023-10-09 22:46:39 -0500

    Intento de quicksort

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -7,15 +7,16 @@
 
 using namespace std;
 
-
 LinkedList::LinkedList()
 {
     this->size = 0;
     this->head = this->tail = nullptr;
 }
-LinkedList::~LinkedList() {
+LinkedList::~LinkedList()
+{
     Node *current = head;
-    while (current != nullptr) {
+    while (current != nullptr)
+    {
         Node *next = current->next;
         delete current;
         current = next;
@@ -25,9 +26,7 @@ LinkedList::~LinkedList() {
     size = 0;
 }
 
-
-
-//Getters
+// Getters
 int LinkedList::length()
 {
     return this->size;
@@ -36,7 +35,7 @@ bool LinkedList::isEmpty()
 {
     return this->size == 0;
 }
-Dato * LinkedList::first()
+Dato *LinkedList::first()
 {
     if (this->size != 0)
     {
@@ -47,7 +46,7 @@ Dato * LinkedList::first()
         throw invalid_argument("No se puede obtener el primer valor de una lista vacía");
     }
 }
-Dato * LinkedList::last()
+Dato *LinkedList::last()
 {
     if (size != 0)
     {
@@ -58,36 +57,36 @@ Dato * LinkedList::last()
         throw invalid_argument("No se puede obtener el último valor de una lista vacía");
     }
 }
-Dato * LinkedList::getAt(int pos)
+Dato *LinkedList::getAt(int pos)
 {
-    //Checar que la lista no esté vacía y la posición esté entre [0,lenght]
-    if (!(this->length() != 0 && pos >= 0 && pos <= this->length()-1) )
+    // Checar que la lista no esté vacía y la posición esté entre [0,lenght]
+    if (!(this->length() != 0 && pos >= 0 && pos <= this->length() - 1))
     {
         throw invalid_argument("Position out of bounds");
     }
-    Node * current = this->head;
+    Node *current = this->head;
     for (int i = 0; i < pos; i++)
     {
         current = current->next;
     }
     return current->data;
-
 }
 
-//Inserts
-void LinkedList::insertFirst(Dato * data) { //Used only if list is empty
+// Inserts
+void LinkedList::insertFirst(Dato *data)
+{ // Used only if list is empty
     this->head = new Node(data, this->head);
-    this->head->prev = nullptr; //si es el primer elemento, su previo es un nullptr
+    this->head->prev = nullptr; // si es el primer elemento, su previo es un nullptr
     this->size++;
     if (size == 1)
     {
-        //Si solo hay un elemento, head y tail son el mismo nodo
+        // Si solo hay un elemento, head y tail son el mismo nodo
         this->tail = this->head;
     }
-
 }
-void LinkedList::insertLast(Dato * data) {
-    //Si la lista está vacía, usamos insertFirst
+void LinkedList::insertLast(Dato *data)
+{
+    // Si la lista está vacía, usamos insertFirst
     if (this->size == 0)
     {
         this->insertFirst(data);
@@ -95,86 +94,108 @@ void LinkedList::insertLast(Dato * data) {
 
     else
     {
-        //Crear nuevo ultimo nodo y ligarlo con el penultimo
+        // Crear nuevo ultimo nodo y ligarlo con el penultimo
         this->tail->next = new Node(data);
 
-        //ligar ultimo con penultimo
+        // ligar ultimo con penultimo
         this->tail->next->prev = this->tail;
 
-        this->tail = this->tail->next; //asignamos tail al actual nuevo ultimo
-        this->tail->data = data;       //asignamos data a el ultimo elemento
+        this->tail = this->tail->next; // asignamos tail al actual nuevo ultimo
+        this->tail->data = data;       // asignamos data a el ultimo elemento
         this->size++;
     }
 }
-void LinkedList::insertAt(int pos, Dato * data) {
+void LinkedList::insertAt(int pos, Dato *data)
+{
     // Si la  posicion es negativa o mayor que la lista, throw error
     if (pos < 0 || pos > (this->size))
     {
         throw invalid_argument("Position out of bounds");
     }
 
-        //Si la pos es 0, usar insertFirst
+    // Si la pos es 0, usar insertFirst
     else if (pos == 0)
     {
         this->insertFirst(data);
     }
 
-        //Si es la última, usar insertLast
+    // Si es la última, usar insertLast
     else if (pos == this->size)
     {
         this->insertLast(data);
     }
 
-        //Si tenemos que insertar entre el nodo 2 y n-1
+    // Si tenemos que insertar entre el nodo 2 y n-1
     else
     {
 
-        Node * currentPtr = head;       //Ptr para recorrer la lista
-        for (int i = 0; i < pos-1;i++)  //POS -1 Para seleccionar el nodo anterior y poder ligar los nodos
+        Node *currentPtr = head;          // Ptr para recorrer la lista
+        for (int i = 0; i < pos - 1; i++) // POS -1 Para seleccionar el nodo anterior y poder ligar los nodos
         {
             currentPtr = currentPtr->next;
         }
 
-        //Guardar posicion del nodo a insertar + 1 en un temp
-        Node * temp = currentPtr->next;
+        // Guardar posicion del nodo a insertar + 1 en un temp
+        Node *temp = currentPtr->next;
 
-        //Insertar nuevo nodo
-        Node * newNode = new Node(data,temp);
+        // Insertar nuevo nodo
+        Node *newNode = new Node(data, temp);
 
         currentPtr->next = newNode;
 
-        //ligar nodo insertado con nodo previo
+        // ligar nodo insertado con nodo previo
         newNode->prev = currentPtr;
-        //ligar nodo insertado + 1 con nodo insertado
+        // ligar nodo insertado + 1 con nodo insertado
         temp->prev = newNode;
 
         this->size++;
-
-
-
     }
 }
-void LinkedList::setAt(int pos, Dato *data) {
-    if(pos < 0 || pos > this->size-1)
+void LinkedList::setAt(int pos, Dato *data)
+{
+    if (pos < 0 || pos > this->size - 1)
     {
         throw invalid_argument("Position out of bounds");
     }
 
-
-    Node * current = this->head; //Ptr para recorrer la  lista
-    for(int i = 0; i < pos; i ++)
+    Node *current = this->head; // Ptr para recorrer la  lista
+    for (int i = 0; i < pos; i++)
     {
         current = current->next;
     }
     current->data = data;
 }
 
+void LinkedList::swap(int pos1, int pos2)
+{
+    if (pos1 < 0 || pos1 > this->size - 1 || pos2 < 0 || pos2 > this->size - 1)
+    {
+        throw invalid_argument("Position out of bounds");
+    }
+
+    Node *current = this->head; // Ptr para recorrer la  lista
+    for (int i = 0; i < pos1; i++)
+    {
+        current = current->next;
+    }
+    Node *current2 = this->head; // Ptr para recorrer la  lista
+    for (int i = 0; i < pos2; i++)
+    {
+        current2 = current2->next;
+    }
+
+    Dato *temp = current->data;
+    current->data = current2->data;
+    current2->data = temp;
+}
+
 // Removes
 
-//Operator overloads
-ostream &operator<<(ostream &os, const LinkedList &list) {
-    Node * current = list.head;
-    for(int i = 0; i < list.size; i++)
+// Operator overloads
+ostream &operator<<(ostream &os, const LinkedList &list)
+{
+    Node *current = list.head;
+    for (int i = 0; i < list.size; i++)
     {
         os << (*current->data) << endl;
         current = current->next;
--- main.cpp
+++ main.cpp
@@ -3,29 +3,68 @@
 #include "LinkedList.h"
 
 using namespace std;
+
+int partition(LinkedList &llDatos, int low, int high)
+{
+    Dato *pivot = llDatos.getAt(high);
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (llDatos.getAt(j) < pivot)
+        {
+            i++;
+            llDatos.swap(i, j);
+        }
+    }
+    llDatos.swap(i + 1, high);
+    return i + 1;
+}
+
+void quicksort(LinkedList &llDatos, int low, int high)
+{
+    if (low < high)
+    {
+        int pivotPos = partition(llDatos, low, high);
+
+        quicksort(llDatos, low, pivotPos - 1);
+        quicksort(llDatos, pivotPos + 1, high);
+    }
+}
+
 int main()
 {
-    string line;         // Var donde se almacenará la linea que se está leyendo del txt
-    LinkedList llDatos; //Linked list donde se almacenarán los datos
+    string line;        // Var donde se almacenará la linea que se está leyendo del txt
+    LinkedList llDatos; // Linked list donde se almacenarán los datos
 
     // leemos el archivo de texto
     cout << "Recolectando Datos del Archivo" << endl;
-    ifstream File("../bitacoraReducida.txt");
+    ifstream File("bitacora.txt");
     // obtenemos cada linea del archivo de texto
     int lines_num = 0;
-    while(getline(File, line)) {
-        //Insertar cada linea como un objecto dato a la Linked List
+    while (getline(File, line))
+    {
+        // Insertar cada linea como un objecto dato a la Linked List
         llDatos.insertLast(new Dato(line));
-        lines_num++; //Aumentar contador de lineas
+        lines_num++; // Aumentar contador de lineas
     }
     File.close();
 
-    //Imprimimos los datos tal como están en el archivo
+    // Imprimimos los datos tal como están en el archivo
     cout << llDatos << endl;
 
-    //Ordenar Datos
+    // Ordenamos los datos
+    cout << "Ordenando Datos" << endl;
+    quicksort(llDatos, 0, llDatos.length() - 1);
 
+    // Imprimimos los datos ordenados
+    cout << llDatos << endl;
 
-    //Ordenar los datos
+    // Guardamos los datos ordenados en un nuevo archivo
+    cout << "Guardando Datos Ordenados" << endl;
+    ofstream File2("bitacora_ordenada.txt");
+    File2 << llDatos;
+    File2.close();
+
     return 0;
 }
